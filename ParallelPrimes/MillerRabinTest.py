a3ff3fb6b1ccc2f633396cb967c1d38ad84252e9
William Llanos <williama.llanos@hotmail.com>
2016-05-30 09:38:04 -0500

    Code formatting

--- ParallelPrimes/MillerRabinTest.py
+++ ParallelPrimes/MillerRabinTest.py
@@ -1,19 +1,14 @@
-from random import randint
-from math import sqrt, ceil, floor, log
-import time
 import sys
 class MillerRabinTest():
-
     def isPrime(self,n,primes,Max):
-     t1=time()
      if n <= Max:
          if n in primes: return True
          return False
      for prime in primes:
-         if n%prime == 0: return False
-     s, r = n-1, 1
-  
-     while not s&1:
+         if n % prime == 0: return False
+
+     s, r = n - 1, 1  
+     while not s & 1:
          s >>= 1
          r  += 1
      for i in primes:    
@@ -21,23 +16,20 @@ class MillerRabinTest():
          if y == 1:
              continue         
          for j in xrange(1,r):   
-             if y == n-1:
+             if y == n - 1:
                  break            
              y = pow(y,2,n)
          else: return False
-       
-            
-   
-
      return True
+
     def isPrime2(self,start, end, prims):
         if end == 2: return 2
   
         n = start
-        m=end-(end+1)%2       
+        m = end - (end + 1) % 2       
         if m < 2: return 1
 
-        primes =0
+        primes = 0
       
 
         v, d, e = int(ceil(log(m,2))) + 2, m, 1
@@ -48,30 +40,30 @@ class MillerRabinTest():
             for i in cent_primes:
                 if i < n: continue
                 if i > m: break
-                primes=primes+1
+                primes = primes + 1
 
-        if n <= 1: n=2
+        if n <= 1: n = 2
 
-        n=n-1
+        n = n - 1
 
-        if n%2 == 0: n=n-1
+        if n % 2 == 0: n = n - 1
 
         for s in xrange(2,v):
             d >>= 1
             e <<= 1
             f, p = e << 1, -e
             
-            h = int(ceil((n-e)/f)) + 1 
-            p += h*f + 1
+            h = int(ceil((n - e) / f)) + 1 
+            p += h * f + 1
 
             rng = xrange(1,s)
             
-            for r in xrange((h*2)+1, d+1,2):
+            for r in xrange((h * 2) + 1, d + 1,2):
                 p += f
-                z = p%6
+                z = p % 6
                 if z != 1 and z != 5: continue
                 for prime in cent_primes:
-                    if p%prime == 0:
+                    if p % prime == 0:
                         break
                 else:
                     
@@ -88,8 +80,8 @@ class MillerRabinTest():
                         else:
                             break
                     else:
-                        primes=primes+1
-        if (start) <= 2: primes=1
+                        primes = primes + 1
+        if (start) <= 2: primes = 1
      
         return primes
  
